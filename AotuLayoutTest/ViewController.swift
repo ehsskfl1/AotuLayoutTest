329e81d3f17c8005b48ab2ebcb26507254d78d43
kim hyunsoo <kimhyunsoo@kimui-MacBook-Pro.local>
2019-01-24 18:44:42 +0900

    Initial Commit

---
+++ AotuLayoutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AotuLayoutTest
+//
+//  Created by kim hyunsoo on 24/01/2019.
+//  Copyright © 2019 kim hyunsoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
